6b7ea800d444c0adeb4eb045866c9a990cf326c9
unknown <lucusapp@gmail.com>
2020-05-12 09:25:23 +0200

    229. Componente Post, Posts y Components Module

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PostsService } from 'src/app/services/posts.service';
+import { Post } from 'src/app/interfaces/interfaces';
 
 @Component({
   selector: 'app-tab1',
@@ -8,13 +9,16 @@ import { PostsService } from 'src/app/services/posts.service';
 })
 export class Tab1Page implements OnInit {
 
+  posts:Post[]=[]
+
   constructor(private postsService:PostsService) {}
 
 
   ngOnInit(){
     this.postsService.getPosts()
                      .subscribe(res=>{
-                       console.log(res);                    
+                       console.log(res);
+                       this.posts.push(...res.posts)                    
                      })   
   }
 
